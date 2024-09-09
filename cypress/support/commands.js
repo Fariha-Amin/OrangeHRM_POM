510fc35cf83a9161381b351adb1594bd02bbf0fa
Fariha-Amin <fariha.amin@dsinnovators.com>
2024-09-09 17:56:33 +0600

    jsdoc installed

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,6 +6,19 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 });
 
 const loginPage = new LoginPage();
+/**
+ * Custom Cypress command to log in a user by providing username and password.
+ *
+ * This command visits the root page of the application, types in the username and password, 
+ * and clicks the login button. It uses methods from the `loginPage` page object to interact 
+ * with the login form.
+ *
+ * @param {Object} values - An object containing login credentials.
+ * @param {string} values.username - The username to log in with.
+ * @param {string} values.password - The password to log in with.
+ * @example
+ * cy.login({ username: 'user@example.com', password: 'password123' });
+ */
 Cypress.Commands.add('login', (values) => {
 
   cy.visit('/')
